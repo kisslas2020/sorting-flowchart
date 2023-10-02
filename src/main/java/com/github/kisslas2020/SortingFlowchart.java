ac223811def4025ab4199803abedc08900721e6b
kisslas2020 <pinewood82@gmail.com>
2023-10-02 13:49:22 +0200

    Implement sortingArray method

--- src/main/java/com/github/kisslas2020/SortingFlowchart.java
+++ src/main/java/com/github/kisslas2020/SortingFlowchart.java
@@ -1,7 +1,26 @@
 package com.github.kisslas2020;
 
-public class SortingFlowchart {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class SortingFlowchart {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[]{1, 2, 56, 32, 51, 2, 8, 92, 15};
+        System.out.println("Original array: " + Arrays.toString(numbers));
+        sortArray(numbers);
+        System.out.println("Sorted array: " + Arrays.toString(numbers));
+    }
+
+    private static void sortArray(int[] numbers) {
+        for(int i = 1; i < numbers.length; i++) {
+            for(int j = 0; j < numbers.length - i; j++) {
+                int num1 = numbers[j];
+                int num2 = numbers[j + 1];
+                if(num1 > num2) {
+                    numbers[j] = num2;
+                    numbers[j + 1] = num1;
+                }
+            }
+        }
     }
 }
